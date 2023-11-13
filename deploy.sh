e7e49c9e8006698e95f3f9c4132edb1dc2d6330f
Faisal <faiisalmalik1996@gmail.com>
2023-11-13 12:08:14 +0000

    updated

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
+#!/bin/bash -l
+
+# Log start of the script
+echo "Starting deployment script..."
 
 # Define the project directory as a variable
 PROJECT_DIR="/var/www/portfolio"
@@ -6,18 +9,44 @@ PROJECT_DIR="/var/www/portfolio"
 # Navigate to your project directory
 cd $PROJECT_DIR
 
+# Print current user
+echo "Running as user: $(whoami)"
+
+#nvm use 16.20.2
+
+# Print Node.js and Yarn versions
+echo "Node version: $(node -v)"
+echo "Yarn version: $(yarn -v)"
+
+# Log environment variables (optional, be cautious with sensitive data)
+# env
+
 # Pull the latest changes from the main branch
+echo "Pulling latest changes from Git..."
 git pull origin main
 
 # Install dependencies
-npm install
-npm audit fix
+echo "Installing dependencies..."
+yarn install
+
 # Build your project (assuming your project has a build script)
-npm run build
+echo "Building the project..."
+yarn build
+
+# Check for build success
+if [ $? -eq 0 ]; then
+    echo "Build succeeded."
+else
+    echo "Build failed. Exiting..."
+    exit 1
+fi
 
 # Empty the /var/www/html directory
+echo "Cleaning the deployment directory..."
 sudo rm -rf /var/www/html/*
 
 # Copy the build artifacts to /var/www/html
-# Adjust the following line according to the actual directory where your build artifacts are generated
+echo "Copying build artifacts to the deployment directory..."
 cp -R $PROJECT_DIR/build/* /var/www/html/
+
+echo "Deployment script finished."
